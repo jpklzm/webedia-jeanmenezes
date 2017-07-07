f035c5307d811507a603a8ad711dbb53fc652714
Jean Menezes <jeanmenezes.nave@gmail.com>
2017-07-07 06:31:26 -0300

    Fix linter

--- front-end/src/components/home/blog/Blog.js
+++ front-end/src/components/home/blog/Blog.js
@@ -9,13 +9,13 @@ const postMock = {
   subTitle: 'Praesent lacinia eget tortor id rutrum. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
   description: 'Aenean et dui molestie, bibendum ipsum id, lobortis ex. Maecenas fringilla leo nibh, vitae placerat leo malesuada sed. Cras pharetra urna vel turpis aliquam aliquet.',
   content: 'Integer felis ipsum, feugiat eget sagittis id, iaculis ut est. Morbi tristique mollis nulla sed eleifend. Aliquam pulvinar sem non dui volutpat imperdiet. Nullam fermentum, arcu non laoreet varius, mi mauris vulputate erat, vitae pharetra sapien augue sit amet ante. Donec vel condimentum libero, at pulvinar diam. Proin sed justo velit. Praesent tellus purus, congue tincidunt sapien ut, venenatis pharetra ligula.',
-  imageUrl: 'http://www.fragil.org/wp-content/themes/courage/assets/images/placeholder_featured_image.svg',
+  imageUrl: 'http://p8.storage.canalblog.com/88/05/1209379/103115257_o.jpg',
 };
 
 class Blog extends Component {
   render() {
     return (
-      <section className="container" style={{width: "80%"}}>
+      <section className="container" style={{ width: '80%' }}>
         <div className="row">
           <FeaturedPost post={postMock} />
         </div>
