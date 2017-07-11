fd26b838c1447640a557f20ad197b3055293db68
Jean Menezes <jeanmenezes.nave@gmail.com>
2017-07-11 03:59:35 -0300

    Add post service for the API @ front

--- front-end/src/services/post.js
+++ front-end/src/services/post.js
@@ -5,3 +5,13 @@ const postsApi = 'http://localhost:9000/v1/posts';
 export function get() {
   return axios.get(postsApi);
 }
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+export function post(data) {
+  return axios.post(postsApi, data, config);
+}
