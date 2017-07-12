1e33463e6afb2f7a773cf2fa1f0c38ec23ccd5f8
Jean Menezes <jeanmenezes.nave@gmail.com>
2017-07-12 18:55:40 -0300

    Add links for posts

--- front-end/src/components/home/blog/post/Post.js
+++ front-end/src/components/home/blog/post/Post.js
@@ -5,30 +5,34 @@ import styles from './styles.scss';
 
 export const FeaturedPost = ({ post }) => {
   return (
-    <div className={styles.featuredCard}>
-      <div className={styles.featuredContainer}>
-        <h1 className={styles.featuredTitle}>{post.title}</h1>
-        <h2 className={styles.description}>{post.description}</h2>
-        <img src={post.imageUrl} className={styles.featuredImage} />
-        <div className={styles.contentArea}>
-          <hr className={styles.hr} />
-          <h1 className={styles.subTitle}>{post.subTitle}</h1>
-          <p>{post.content}</p>
+    <a href="#">
+      <div className={styles.featuredCard}>
+        <div className={styles.featuredContainer}>
+          <h1 className={styles.featuredTitle}>{post.title}</h1>
+          <h2 className={styles.description}>{post.description}</h2>
+          <img src={post.imageUrl} className={styles.featuredImage} />
+          <div className={styles.contentArea}>
+            <hr className={styles.hr} />
+            <h1 className={styles.subTitle}>{post.subTitle}</h1>
+            <p>{post.content}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
 export const Post = ({ post }) => {
   return (
     <div className="col-sm-6">
-      <div className={styles.card}>
-        <div className={styles.container}>
-          <h1 className={styles.title}>{post.title}</h1>
-          <img src={post.imageUrl} className={styles.image} />
+      <a href="#">
+        <div className={styles.card}>
+          <div className={styles.container}>
+            <h1 className={styles.title}>{post.title}</h1>
+            <img src={post.imageUrl} className={styles.image} />
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
