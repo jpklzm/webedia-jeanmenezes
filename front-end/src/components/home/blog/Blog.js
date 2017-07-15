2c49215aee01b818704fc6ce9a081278c75b4c4d
Jean Menezes <jeanmenezes.nave@gmail.com>
2017-07-14 21:05:27 -0300

    Add styles for pagination buttons

--- front-end/src/components/home/blog/Blog.js
+++ front-end/src/components/home/blog/Blog.js
@@ -4,6 +4,26 @@ import PropTypes from 'prop-types';
 import { get, getOne } from '../../../services/post';
 import { FeaturedPost, Post } from './post/Post';
 
+const buttonLeft = {
+  backgroundColor: '#fe783b',
+  border: 'none',
+  color: 'white',
+  padding: '16px 12px',
+  textDecoration: 'none',
+  margin: '24px 10px',
+  cursor: 'pointer',
+};
+const buttonRight = {
+  backgroundColor: '#fe783b',
+  border: 'none',
+  color: 'white',
+  padding: '16px 12px',
+  textDecoration: 'none',
+  margin: '24px 10px',
+  cursor: 'pointer',
+  float: 'right',
+};
+
 class Blog extends Component {
   constructor(props) {
     super(props);
@@ -42,20 +62,6 @@ class Blog extends Component {
   render() {
     return (
       <section className="container" style={{ width: '80%' }}>
-        {parseInt(this.props.page) > 1
-          ? <button
-              onClick={() => (window.location = 'http://localhost:4000/blog/page/' + (parseInt(this.props.page) - 1))}
-            >
-              Página Anterior
-            </button>
-          : null}
-        {this.state.posts.length === 5
-          ? <button
-              onClick={() => (window.location = 'http://localhost:4000/blog/page/' + (parseInt(this.props.page) + 1))}
-            >
-              Próxima página
-            </button>
-          : null}
         <article className="row">
           {this.state.posts[0] ? <FeaturedPost post={this.state.posts[0]} /> : null}
         </article>
@@ -75,6 +81,26 @@ class Blog extends Component {
                 </article>
               ))}
             </div>}
+        <div className='row'>
+          {parseInt(this.props.page) > 1
+            ? <button
+                style={buttonLeft}
+                className='col-sm-3'
+                onClick={() => (window.location = 'http://localhost:4000/blog/page/' + (parseInt(this.props.page) - 1))}
+              >
+                Página Anterior
+              </button>
+            : null}
+          {this.state.posts.length === 5
+            ? <button
+                style={buttonRight}
+                className='col-sm-3'
+                onClick={() => (window.location = 'http://localhost:4000/blog/page/' + (parseInt(this.props.page) + 1))}
+              >
+                Próxima página
+              </button>
+            : null}
+        </div>
       </section>
     );
   }
